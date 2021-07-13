11cd27b2931799e1a77d92af97aa2c49c3879193
michaelbragg <michael@michaelbragg.net>
2021-07-13 12:07:13 +0100

    Call snapshot on visit test

--- cypress/integration/00 Setup/00-request.js
+++ cypress/integration/00 Setup/00-request.js
@@ -7,6 +7,26 @@ describe( 'Smoke tests', function () {
 			.its('status')
 			.should('be.equal', 200);
 
+	});
+
+	// Check Fresh theme is running.
+	it('Should have theme active.', function () {
+
+		cy.visit( '/' );
+
+		cy.get( 'body' )
+			.should( 'have.class', 'is-light-theme' );
+
+	});
+
+	// Check bottom of page is loaded.
+	it('Should load footer.', function () {
+
+		cy.visit( '/' );
+
+		cy.get('[role=contentinfo]')
+			.should( 'exist' );
+
 		cy.percySnapshot( 'Front Page', { widths: [320, 1024] } );
 
 	});
