e29a461d5abd7ca83050f427a5bec2493e95ed39
Michael Bragg <michael@michaelbragg.com>
2022-05-26 11:03:09 +0000

    [Added] PHP Stan stub file for WP Hook

---
+++ tests/phpstan/constants.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * PHP Stan: Constants
+ *
+ * @package Vatu\WordPress\Demo\Tests
+ */
+
+declare(strict_types=1);
+
+// phpcs:disable Squiz.PHP.DiscouragedFunctions,NeutronStandard.Constants.DisallowDefine
