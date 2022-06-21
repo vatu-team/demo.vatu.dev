4073bd98f3e06b4518a235ba27e79641d55e05b5
Michael Bragg <michael@michaelbragg.com>
2022-06-21 16:30:22 +0000

    [Upgraded] PHPStan Config

---
+++ tests/phpstan/constants.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * PHP Stan: Constants
+ *
+ * @package Vatu\Tests\PHPStan
+ */
+
+declare(strict_types=1);
+
+// phpcs:disable Squiz.PHP.DiscouragedFunctions,NeutronStandard.Constants.DisallowDefine
