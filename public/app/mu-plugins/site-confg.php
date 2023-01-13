1fe613200a65f880439e2c61fc6ace1cf2b665e4
Michael Bragg <michael@michaelbragg.com>
2023-01-13 11:25:48 +0000

    [Added] Site Config file to MU Plugins

---
+++ public/app/mu-plugins/site-confg.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Site Specific Configuration
+ * Plugin URI: https://vatu.dev
+ * Description: Site settings independent to the a theme or plugin.
+ * Version: 1.0.0
+ * Author: Vatu
+ * Author URI: https://vatu.dev/
+ * License: GPL3
+ *
+ * @package   Vatu/WordPress/Plugin/SiteConfig
+ * @copyright 2023 Vatu
+ */
