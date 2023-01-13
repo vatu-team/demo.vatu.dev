9fec41022edd4e5af22f0200bfb49c211fd1dfca
Michael Bragg <michael@michaelbragg.com>
2023-01-13 11:28:27 +0000

    [Added] Disable site health tests for debug and background updates

--- public/app/mu-plugins/site-confg.php
+++ public/app/mu-plugins/site-confg.php
@@ -16,11 +16,25 @@
  * Disable indexing for development sites.
  */
 if (
-    defined( 'WP_ENVIRONMENT_TYPE' ) &&
-    in_array( WP_ENVIRONMENT_TYPE, [ 'local', 'development', 'qa', 'staging' ] )
+	defined( 'WP_ENVIRONMENT_TYPE' ) &&
+	in_array( WP_ENVIRONMENT_TYPE, [ 'local', 'development', 'qa', 'staging' ] )
 ) {
-    add_action(
-        'pre_option_blog_public',
-        '__return_zero'
-    );
+	add_action(
+		'pre_option_blog_public',
+		'__return_zero'
+	);
 }
+
+/**
+ * Remove warning about site health statuses.
+ */
+function vatu_disable_site_health_tests( array $tests ) {
+	unset( $tests['direct']['debug_enabled'] );
+	unset( $tests['async']['background_updates'] );
+	return $tests;
+}
+
+add_filter(
+	'site_status_tests',
+	'vatu_disable_site_health_tests',
+);
